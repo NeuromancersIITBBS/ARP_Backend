68994187d4505140aeb7f015e189c96eed440996
Rishabh <gupta.rishabh606@gmail.com>
2019-10-20 11:49:43 +0530

    Edited config.js and renamed firebase.js to firebasedb.js

--- firebasedb.js
+++ firebasedb.js
@@ -1,7 +1,9 @@
 let firestore = firebase.firestore();
+
 const srCS = firestore.doc('/branch/CS/');
 const srEE = firestore.doc('/branch/EE/');
 const srEC = firestore.doc('/branch/CS/');
 const srME = firestore.doc('/branch/EE/');
 const srCS = firestore.doc('/branch/CS/');
+
 const studyResourceEE = firestore.doc('/branch/EE/');
