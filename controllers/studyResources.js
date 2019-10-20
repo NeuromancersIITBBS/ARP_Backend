fffbb95249b13b1522886e79f468dca19e2ddd2c
Rishabh <gupta.rishabh606@gmail.com>
2019-10-20 15:30:36 +0530

    wrote get flagged resources

--- controllers/studyResources.js
+++ controllers/studyResources.js
@@ -1,21 +1,32 @@
 const studyResRouter = require('express').Router();
 const resSchema = require('../models/firebasedb.js');
-const studyResources = require('../models/firebasedb.js')
+const studyResources = require('../models/firebasedb.js').studyResources;
+const schema = require('../models/firebasedb.js').schema;
 
-//get all resources for admin
-studyResRouter.get('/',(req,res)=>{
-studyResources
+//get all flagged resources for admin
+studyResRouter.get('/', async (req,res)=>{
+    let list = [];
+    // studyResources.get().then((branches)=>{
+    //    branches.forEach((branch)=>{
+    //         branch.getCollections().then((subjects)=> {
+    //             subjects.forEach((subject) => {
+    //                 subject.where('flag','>',"0").get().then((resource)=>{
+    //                     list.push(resource);
+    //                 })
+    //             })
+    //         })
+    //    })
+    // });
+    let branches = await studyResources.get();
+    for (const branch of branches) {
+        let subjects = await branch.getCollections();
+        for (const subject of subjects) {
+           let resource = await subject.where('flag','>',"0").get();
+           list.push(resource);
+        }
+    }
+    res.status(200).send(list);
 });
-//get resource by subjectcode
-studyResRouter.get('/:branch/subjects/:subjectCode',(req,res)=>{
-
-});
-
-//upload resources of a subject code
-studyResRouter.post('/:branch/subjects/:subjectCode',(req,res)=>{
-
-});
-
 
 //get all subjects of a branch
 studyResRouter.get('/:branch/subjects',(req,res)=>{
@@ -23,16 +34,18 @@ studyResRouter.get('/:branch/subjects',(req,res)=>{
 });
 
 //get resource by subjectcode
-studyResRouter.get('/:branch/subjects/:subjectCode/resources/:uniqueId',(req,res)=>{
+studyResRouter.get('/:branch/subjects/:subjectCode',(req,res)=>{
 
 });
 
+studyResRouter.put('/:branch/subjects/:subjectCode/resources/:uniqueId',(req,res)=>{
+    studyResources.doc("")
+});
 
-studyResRouter.post('/:branch/subjects/:subjectCode/resources/:uniqueId',(req,res)=>{
+//upload resources of a subject code
+studyResRouter.post('/:branch/subjects/:subjectCode',(req,res)=>{
 
 });
-studyResRouter.post('/:branch/subjects/:subjectCode/resources/:uniqueId',(req,res)=>{
 
-});
 
 module.exports = studyResRouter;
