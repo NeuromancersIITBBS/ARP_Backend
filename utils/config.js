3e9415489e32be39be16a9184989c0ba2458a0b9
Rishabh <gupta.rishabh606@gmail.com>
2019-10-21 15:00:23 +0530

    Added api key in .env

--- utils/config.js
+++ utils/config.js
@@ -5,15 +5,15 @@ require('dotenv').config('../config_num.env')
 
 let PORT = process.env.PORT||3001;
 console.log(PORT);
-let firebaseConfig = {
-    apiKey: "AIzaSyAA3gAByQRr6WNhmHpw8UtVr0Civi46Ork\n",
-    authDomain: "academic-resource-portal-b.firebaseapp.com",
-    databaseURL: "https://academic-resource-portal-b.firebaseio.com",
-    projectId: "academic-resource-portal-b",
-    storageBucket: "academic-resource-portal-b.appspot.com",
-    messagingSenderId: "750397614411",
-};
 
+let firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+};
 firebase.initializeApp(firebaseConfig);
 firebase.firestore().settings({timestampsInSnapshots:true});
 
